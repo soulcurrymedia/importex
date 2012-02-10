ee52a25ffd3def7cb3e3df6540f680360976483e
David J. Brenes <david.brenes@simplelogica.net>
2012-02-10 10:33:56 +0100

    Introducing procs for more complex translations and fields for  simple translation where the fields in the class have no the same name as the excel

--- spec/importex/translation_spec.rb
+++ spec/importex/translation_spec.rb
@@ -19,6 +19,7 @@ describe Importex::Base do
     ImportClass1.translated_class.should == DummyClass
   end
 
+
   it "should translate all rows" do
     ImportClass1.column "Name"
     ImportClass1.column "Age", :type => Integer
@@ -28,20 +29,17 @@ describe Importex::Base do
 
     dummy_objects = ImportClass1.translate_all
 
-    attributes = [
+    rows = [
       {"Name" => "Foo", "Age" => 27}, # First row
       {"Name" => "Bar", "Age" => 42}, # Second row
       {"Name"=>"Blue", "Age"=>28}, # Third row
-      {"Name" => "", "Age" => 25} # Fourth row
+      {"Name" => nil, "Age" => 25} # Fourth row
     ]
 
-    dummy_objects.each_with_index do |dummy_object, i|
-      attributes[i].each do |name, value|
-        dummy_object.send(name.downcase).should == value
-      end
-    end
+    check_rows rows, dummy_objects
 
   end
+
 
   it "should translate only valid rows when required" do
     ImportClass1.column "Name", :validate_presence => true
@@ -52,25 +50,19 @@ describe Importex::Base do
 
     dummy_objects = ImportClass1.translate_valid
 
-    attributes = [
+    rows = [
       {"Name" => "Foo", "Age" => 27}, # First row
       {"Name" => "Bar", "Age" => 42}, # Second row
       {"Name"=>"Blue", "Age"=>28}, # Third row
     ]
 
-    dummy_objects.length.should == 3
-
-    dummy_objects.each_with_index do |dummy_object, i|
-      attributes[i].each do |name, value|
-        dummy_object.send(name.downcase).should == value
-      end
-    end
-
+    check_rows rows, dummy_objects
 
   end
 
+
   it "should translate only valid rows when required" do
-        ImportClass1.column "Name", :validate_presence => true
+    ImportClass1.column "Name", :validate_presence => true
     ImportClass1.column "Age", :type => Integer
     ImportClass1.translate_to DummyClass.name
 
@@ -78,20 +70,45 @@ describe Importex::Base do
 
     dummy_objects = ImportClass1.translate_invalid
 
-    attributes = [
+    rows = [
       {"Name" => nil, "Age" => 25} # Fourth row
     ]
 
-    dummy_objects.length.should == 1
-
-    dummy_objects.each_with_index do |dummy_object, i|
-      attributes[i].each do |name, value|
-        dummy_object.send(name.downcase).should == value
-      end
-    end
-
+    check_rows rows, dummy_objects
 
   end
 
 
+  it "should translate fields using the :field parameter" do
+    ImportClass1.column "Rank", :translation => :name
+    ImportClass1.column "Age", :type => Integer
+    ImportClass1.translate_to DummyClass.name
+
+    ImportClass1.import(@xls_file)
+
+    dummy_objects = ImportClass1.translate_all
+
+    rows = [
+      {"Name" => 1, "Age" => 27}, # First row
+      {"Name" => 2, "Age" => 42}, # Second row
+      {"Name"=>nil, "Age"=>28}, # Third row
+      {"Name" => 3, "Age" => 25} # Fourth row
+    ]
+
+    check_rows rows, dummy_objects
+
+  end
+
+
+end
+
+def check_rows rows, objects
+
+    objects.length.should == rows.count
+
+    objects.each_with_index do |dummy_object, i|
+      rows[i].each do |name, value|
+        dummy_object.send(name.downcase).should == value
+      end
+    end
 end
